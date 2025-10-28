4f0feb9dc22f48e4a12fa4dc3c0063616d1cb328
cindyt0526 <cindyt0526@gmail.com>
2025-10-27 21:55:17 -0400

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: '/cindyt0526.github.io/',
 })
